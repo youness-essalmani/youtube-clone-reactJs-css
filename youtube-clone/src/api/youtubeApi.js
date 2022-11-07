646babd2d3c13f561a86117d5670600b0d05cd81
youness-essalmani <117415679+youness-essalmani@users.noreply.github.com>
2022-11-07 01:35:53 +0100

    Update youtubeApi.js

--- youtube-clone/src/api/youtubeApi.js
+++ youtube-clone/src/api/youtubeApi.js
@@ -6,13 +6,5 @@ export const YouTubeAPI = axios.create({
 
 export const API_KEY = 'AIzaSyDPbDb26Mrc_vV9asHaocaBHJWhfXNtZQ0'; // mention your youtube API key here
 
-export const fetchChannels = YouTubeAPI.get(
-    {
-    params: {
-        part: 'snippet,id,localizations,statistics,contentDetails',
-        maxResults: 12,
-        key: API_KEY
-    }
-})
 
 
