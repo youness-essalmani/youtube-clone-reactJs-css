4b835d7f6864370b8e8cc8bda4452f68492393de
youness-essalmani <117415679+youness-essalmani@users.noreply.github.com>
2022-11-07 02:54:23 +0100

    Update Search.js

--- youtube-clone/src/pages/Search.js
+++ youtube-clone/src/pages/Search.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import Navbar from "../component/Navbar"
 import SearchArea from "../component/SearchArea";
-import { useSelector } from 'react-redux';
 
 function Search() {
-
-  const { videos } = useSelector((store) => store.home);
-
-  console.log(videos);
-
+  
   return (
-    
     <div>
       <Navbar/>
       <SearchArea/>
