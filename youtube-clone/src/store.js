d5b57ebd8bbd9173a28a003c6582a14cc9d3bfe1
youness-essalmani <117415679+youness-essalmani@users.noreply.github.com>
2022-11-07 01:37:56 +0100

    Update store.js

--- youtube-clone/src/store.js
+++ youtube-clone/src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import  viedoReducer from './features/video/videoSlice'
-import modalReducer from './features/sidebar/sidebarSlice'
 
 export const store = configureStore({
     reducer:{
         home:viedoReducer,
-        sidebar:modalReducer,
     },
 });
 
