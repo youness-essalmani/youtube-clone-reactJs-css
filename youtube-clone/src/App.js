6d189217079e08afa87ed4d81f010a7ec1f8f8ce
youness-essalmani <117415679+youness-essalmani@users.noreply.github.com>
2022-11-07 02:53:00 +0100

    Update App.js

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-// import { Switch } from 'react-router-dom';
 import Home from "./pages/Home";
 import Search from './pages/Search';
 import Watch from './pages/Watch'
