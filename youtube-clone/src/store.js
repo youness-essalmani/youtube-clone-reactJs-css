c39bb387307e75f47171d3152a1d3651554eebe3
youness-essalmani <117415679+youness-essalmani@users.noreply.github.com>
2022-11-10 20:48:49 +0100

    Delete youtube-clone directory

--- youtube-clone/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import  viedoReducer from './features/video/videoSlice'
-
-export const store = configureStore({
-    reducer:{
-        video:viedoReducer,
-    },
-});
-
